203c4e9fb2d2183ecbd4175cc9981a4da6980933
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-03-21 16:37:12 +0300

    node js titorial

---
+++ app.js
@@ -0,0 +1,67 @@
+// //step1;We’re setting a variable express that refers to the Express package and then instantiating it in another variable called app
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+// const path = require('path');
+// const router = express.Router() ;//use inbuilt routing function to create routes
+
+// /*step3; We’re using Express’s get method to create our route.
+//  We specify that when a visitor hits the homepage at the route /,
+//   accept both a request and a response which by convention are shortened to req and res.
+//    Note that these are positional arguments so you could call them x and y if you want and
+//  whichever one was first would represent the request object and the second one the response object.*/
+// //app.get('/', (req, res) => { // new
+// //    res.send('Homepage! Hello world.');
+// //  });
+//  /* //route to about page
+//   app.get('/about', (req, res) => { // new
+//     res.send('About page. Nice.');
+//   });*/
+//   //route to home page
+//     // app.get('/', (req, res) => { // new
+//     //   res.sendFile(__dirname + '/index.html');
+//     // });
+ 
+// //add router
+// app.use('/', 'router')
+// =========================================================
+const express = require('express');
+const {resolve} = require('path');
+const port = 3000;
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+//TO VIEW PUG
+app.set("view engine","pug")
+app.set("views", path.join(__dirname,"views"))
+
+
+
+
+
+
+
+
+
+
+// ===================================
+//__dirname will resolve project folder
+router.get("/", (req,res)=>(
+  res.sendFile(path.join(__dirname + "/index.html"))
+));
+
+router.get("/about", (req,res)=>(
+  res.sendFile(path.join(__dirname + "/about.html"))
+));
+
+
+
+
+//add the router
+app.use("/",router)
+app.use("/about",router)
+
+
+//step2;set up a server
+app.listen(port, ()=> console.log(`Listening on port ${port}`))
---
+++ readme.md
@@ -0,0 +1,12 @@
+uses express to route and it was installed using `npm install express`
+using nodemon to auto restart my server, intsalled using `npm install nodemon`
+
+#PUG
+npm install pug
+we use pug to process data instead of html
+no tags
+acknowlede indentation to catter for tags
+use dot to create a class e.g <p class="home"></p> is same as p.home 
+for a div we a hash(#) instaed of a dot(.)
+a class or id that does not start with anything is recognised as a div.
+ p.home#child This is pug   has class home, id child
